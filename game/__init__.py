bd9f3e943d0f64cecf125076b0160ae87424953c
Ilandrm <ilandarmon28@gmail.com>
2024-09-05 16:52:54 +0200

    player and movement

--- game/__init__.py
+++ game/__init__.py
@@ -1,6 +1,6 @@
 import pygame
 from objects import GameState
-
+from objects.player import Player
 pygame.init()
 screen = pygame.display.set_mode((1024, 768))
 clock = pygame.time.Clock()
@@ -9,6 +9,8 @@ dt = 0
 gamestate = GameState.GameState.ENTRYPOINT
 window = pygame.display.set_mode((1024, 768))
 background = pygame.image.load("../assets/menu-background.png")
+player = Player(1200,1200)
+player.image = pygame.transform.scale(player.image, (100, 100))
 while running:
     if gamestate == GameState.GameState.ENTRYPOINT:
         clock.tick(60)
@@ -17,5 +19,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    player.deplacer()
     window.blit(background, (0, 0))
+    window.blit(player.image, player.rect)
     pygame.display.update()
---
+++ game/objects/player.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load("assets/testPlayer.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.vitesse = 7
+
+    def deplacer(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.rect.x >0:
+            self.rect.x -= self.vitesse
+        elif keys[pygame.K_RIGHT]and self.rect.x <1000:
+            self.rect.x += self.vitesse
+        elif keys[pygame.K_UP]and self.rect.y >0:
+            self.rect.y -= self.vitesse
+        elif keys[pygame.K_DOWN]and self.rect.y <744:
+            self.rect.y += self.vitesse
+
+
+
+
+

dfede4a068a44b4df9cbfc0ca06ed957dbb11d3b
Ilandrm <ilandarmon28@gmail.com>
2024-09-06 10:12:16 +0200

    gameSTate update

--- game/objects/GameState.py
+++ game/objects/GameState.py
@@ -2,4 +2,9 @@ from enum import Enum
 
 class GameState(Enum):
     ENTRYPOINT = 0
-    SELECTING_COUNTRY = 1
+    WAITING_START_SCREEN = 1
+    CHOOSE_PSEUDO = 2
+    FIND_SOMEONE = 3
+    SELECTING_COUNTRY = 4
+    START_GAME = 5
+    IN_GAME = 6

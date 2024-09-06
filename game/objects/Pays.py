00c9cd0b8f39bb928c3c55c81dc224db0577efa8
lynirl <rachel.peretti@etu.univ-grenoble-alpes.fr>
2024-09-06 10:45:05 +0200

    added small multiplier function

--- game/objects/Pays.py
+++ game/objects/Pays.py
@@ -1,5 +1,13 @@
 class Pays:
     def __init__(self, nom, competences):
-        self.nom = nom
-        self.competences = competences
+        self.nom = nom #string
+        self.competences = competences #dictionnaire
 
+    #définit le multiplier pour une compétence
+    def setMultiplier(self, competence, multiplier):
+        #si la compétence existe
+        if self.competences.has_key(competence):
+            #on change son multiplier
+            self.competences[competence] = multiplier
+        else:
+            return

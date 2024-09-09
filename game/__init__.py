258c983ed7d8c3c316e437d0dad0bcd23ab7c39b
Ilandrm <ilandarmon28@gmail.com>
2024-09-09 08:39:07 +0200

    Removed problematic commit

--- game/__init__.py
+++ game/__init__.py
@@ -1,8 +1,9 @@
 import pygame
+import pytmx
 from objects import GameState
 from objects.player import Player
 from objects.button import Button
-
+from objects.Pays import Pays
 pygame.init()
 window_width = 1024
 window_height = 768
@@ -18,10 +19,22 @@ textFont = pygame.font.Font("./assets/fonts/RETROTECH.ttf", 48)
 parentheseFont = pygame.font.Font("./assets/fonts/RETROTECH.ttf", 24)
 GRIS = (229, 231, 230)
 ORANGE_PALE = (238, 230, 216)
-startButton = Button(window_width / 1.5, window_height / 1.25, pygame.image.load("./assets/btn_start.png"))
-nameButton =  Button(window_width / 1.5, window_height / 1.25, pygame.image.load("./assets/btn_valider.png"))
+startButton = Button(window_width / 1.6, window_height / 1.25, pygame.image.load("./assets/btn_start.png"))
+nameButton =  Button(window_width / 1.6, window_height / 1.25, pygame.image.load("./assets/btn_valider.png"))
 startButton.image = pygame.transform.scale(startButton.image,(246,78))
 nameButton.image = pygame.transform.scale(nameButton.image,(246,78))
+tmx_data = pytmx.util_pygame.load_pygame("assets/tileset/1.tmx")
+
+def draw_map(screen, tmx_data):
+    tile_width = tmx_data.tilewidth
+    tile_height = tmx_data.tileheight
+
+    for layer in tmx_data.visible_layers:
+        if isinstance(layer, pytmx.TiledTileLayer):
+            for x, y, gid in layer:
+                tile = tmx_data.get_tile_image_by_gid(gid)
+                if tile:
+                    screen.blit(tile, (x * tile_width, y * tile_height))
 
 def initMenu():
     background = pygame.image.load("./assets/menu-background.png")
@@ -51,14 +64,19 @@ def choosePseudo(name):
     window.blit(parentheseText,parentheseText_rect)
     nameButton.draw(window)
 
-def selectCOuntry():
-    titletext = titlefont.render("Choisir un pseudo", False, (0, 0, 0))
+def selectCountry():
+    titletext = titlefont.render("Selectionner un pays", False, (0, 0, 0))
     titletext_rect = titletext.get_rect()
-    titletext_rect.center = (window_width / 2, window_height / 4)
+    titletext_rect.center = (window_width / 2, window_height / 8)
+    window.fill(ORANGE_PALE)
+    pygame.draw.rect(window, GRIS , (window_width /8.5, window_height / 4, 256, 128))
+    pygame.draw.rect(window, GRIS , (window_width /2.55, window_height / 4, 256, 128))
+    pygame.draw.rect(window, GRIS , (window_width /1.5 , window_height / 4, 256, 128))
+    pygame.draw.rect(window, GRIS , (window_width /8.5, window_height / 2, 256, 128))
+    pygame.draw.rect(window, GRIS , (window_width /2.55, window_height / 2, 256, 128))
+    pygame.draw.rect(window, GRIS , (window_width /1.5 , window_height / 2, 256, 128))
+    window.blit(titletext, titletext_rect)
 while running:
-    if gamestate == GameState.GameState.ENTRYPOINT:
-        initMenu()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -67,23 +85,22 @@ while running:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_BACKSPACE:
                     name = name[:-1]
-                elif len(name)<= 18:
+                elif len(name) <= 18 and event.unicode.isprintable():
                     name += event.unicode
 
-    if startButton.isClicked():
-        gamestate = GameState.GameState.CHOOSE_PSEUDO
-    if gamestate == GameState.GameState.CHOOSE_PSEUDO:
+    if gamestate == GameState.GameState.ENTRYPOINT:
+        initMenu()
+        if startButton.isClicked():
+            gamestate = GameState.GameState.CHOOSE_PSEUDO
+
+    elif gamestate == GameState.GameState.CHOOSE_PSEUDO:
         choosePseudo(name)
+        if nameButton.isClicked() and name != "":
+            gamestate = GameState.GameState.SELECTING_COUNTRY
 
-    if nameButton.isClicked():
-        gamestate = GameState.GameState.SELECTING_COUNTRY
-    player.deplacer()
-
-
-
-    if gamestate == GameState.GameState.SELECTING_COUNTRY:
-        selectCOuntry()
-
+    elif gamestate == GameState.GameState.SELECTING_COUNTRY:
+        selectCountry()
 
     pygame.display.update()
     clock.tick(60)
+

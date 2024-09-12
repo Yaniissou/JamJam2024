6e9a80afcefdd2b2c85246270d32aa6b869cc5f2
Yaniissou <yanissoumc@gmail.com>
2024-09-12 13:53:04 +0200

    started checking for keys

--- game/objects/QTE.py
+++ game/objects/QTE.py
@@ -10,6 +10,14 @@ class QTE:
     #fonction qui s'exécute à la fin du qte
     def start(self,window_width,window_height,color,screen):
         pygame.draw.rect(screen,color,(window_width/4, window_height/4,window_width/2, window_height/2))
+        print("début d'un qte")
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    print('action effectuée')
+                return
+
+
         # Si on atteint cet endroit, le temps est écoulé
         self.onEnd()
 
